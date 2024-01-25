830ec2a25769181a147be7e9c704b83121c9921c
wildan <wildan78933@gmail.com>
2024-01-25 12:54:15 +0700

    add talents

--- src/routes/TalentsRoute.jsx
+++ src/routes/TalentsRoute.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import Talents from "../pages/talents";
-// import Create from "../pages/talents/create";
-// import Edit from "../pages/talents/edit";
+import Create from "../pages/talents/create";
+import Edit from "../pages/talents/edit";
 
 export function TalentsRoute() {
   return (
     <Routes>
       <Route path="/" element={<Talents />} />
-      {/* <Route path="/create" element={<Create />} />
-      <Route path="/edit/:talentId" element={<Edit />} /> */}
+      <Route path="/create" element={<Create />} />
+      <Route path="/edit/:talentId" element={<Edit />} />
     </Routes>
   );
 }
