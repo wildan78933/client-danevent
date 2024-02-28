871cd14c6871d40e4b34e310fb876ab345daff93
wildan <wildan78933@gmail.com>
2024-02-28 08:53:08 +0700

    deploy

--- src/configs/index.jsx
+++ src/configs/index.jsx
@@ -1,5 +1,6 @@
 const config = {
-  api_host_dev: import.meta.env.VITE_REACT_APP_HOST_DEV,
+  // api_host_dev: import.meta.env.VITE_REACT_APP_HOST_DEV,
+  api_host_dev: import.meta.env.VITE_REACT_APP_HOST_DEV + "/api/v1",
   api_image: import.meta.env.VITE_REACT_APP_HOST_IMAGE_DEV,
 };
 
